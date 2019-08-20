dae11df7059db1aca2bf2cd7b8afdbf5537ad8d8
Ashley <a.j.morahan@gmail.com>
2019-08-20 16:38:55 +0100

    Looping optim
    it kinda works

--- OptimiseSnks.m
+++ OptimiseSnks.m
@@ -1,14 +1,19 @@
 % Optimise Snakes 
 tic;
 band = [33 40 48 60 74 88 97 109 122 130 140 154 166 175 187 200 212 219 227]; 
-sigma = 0.5;
-plim = 30;
+%band = [105 120];
+dim = length(band);
+sigma = 1;
+plim = 5;
 % 
-x = 22:30:478;
+x = 22:48:478;
 % 
 %y = randi([-3,3],1,length(x));
-y = zeros([19,length(x)]);
-    
+%y = zeros([dim,length(x)]);
+
+%msk = maskData(mskNd4X,0.1);
+msk = maskData(Nd4_XLin,0.1);
+
 %SolidSnake(y,x,band,TstMskArr,sigma,0);
 % % 
 % h = optimset('MaxFunEvals',20000, 'Algorithm', 'levenberg-marquardt',...
@@ -19,20 +24,24 @@ y = zeros([19,length(x)]);
 
 % 
 h = optimset('MaxFunEvals',1000, 'Algorithm', 'levenberg-marquardt',...
-        'TolX',1e-10,'TolFun',1e-10,'Display','off','FinDiffRelStep',1);
+        'TolX',1e-10,'TolFun',1e-10,'Display','off','FinDiffRelStep',0.1);
 %now run the fitting
+resmin = 4e+07;
+
+for i= 1:10
+y = ones(dim, length(x)).*rand;
 
 %[NewY, RESNORM,EXITFLAG,OUTPUT] = fminunc('SolidSnake',y,h,x,band,SPLN,sigma,0,plim);
-%msk = maskData(mskNd4X,0.01);
-msk = maskData(Nd4_XLin,0.000001);
-%mskArray = reshape(msk, 1, []);
 [NewY, RESNORM,EXITFLAG,OUTPUT] = fminunc('SolidSnake',y,h,x,band,msk,sigma,0,plim);
+    if RESNORM <= resmin
+        resmin = RESNORM;
+        minY = NewY;
+    end
+end 
 
+%SolidSnake(NewY,x,band,SPLN,sigma,4,plim);
 
-
-%NY = SolidSnake(NewY,x,band,SPLN,sigma,4,plim);
-
-NY = SolidSnake(NewY,x,band,msk,sigma,4,plim);
+SolidSnake(minY,x,band,msk,sigma,4,plim);
 
 %%
 %figure, imagesc(SPLN);
