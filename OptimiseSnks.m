e5d9e2e31673da396ad59f372296618ff49632f3
Ashley <a.j.morahan@gmail.com>
2019-08-28 17:36:45 +0100

    3 Part Optim
    Optimize with 3 parts process
    1 loop of Rndm Start and SolidSnake
    1 Loop of SolidSnake
    1 SolidSnake

--- OptimiseSnks.m
+++ OptimiseSnks.m
@@ -8,7 +8,7 @@ band = round(band);
 dim = length(band);
 sigma = 1;
 plim = 10;
-% =]#
+%
 x = 22:28:478;
 % 
 %y = randi([-3,3],1,length(x));
@@ -33,36 +33,37 @@ resmin = 4e+07;
 
 for i= 1:5
 y = ones(dim, length(x)).*rand;
-
+band = round(band + (randi([-150 150],1,19)./100)');
 %[NewY, RESNORM,EXITFLAG,OUTPUT] = fminunc('SolidSnake',y,h,x,band,SPLN,sigma,0,plim);
-[NewY, RESNORM,EXITFLAG,OUTPUT] = fminunc('SolidSnake',y,h,x,band,msk,sigma,0,plim);
+[NewY, RESNORM,~,~] = fminunc('SolidSnake',y,h,x,band,msk,sigma,0,plim);
     if RESNORM <= resmin
         resmin = RESNORM;
         minY = NewY;
         minBand = band;
     end
 end 
+SolidSnake(minY,x,minBand,msk,sigma,4,plim);
 
-h2 = optimset('MaxFunEvals',1000, 'Algorithm', 'levenberg-marquardt',...
-        'TolX',1e-10,'TolFun',1e-10,'Display','off','FinDiffRelStep',1);
-for i= 1:5
-    
-    band = round(band + (randi([-150 150],1,19)./100)');
-    [NewBand, RESNORM,EXITFLAG,OUTPUT] = fminunc('LiquidSnake',band,h2,x,minY,msk,sigma,0,plim);
-    if RESNORM <= resmin
-        resmin = RESNORM;
-        minBand = band;
-    end
-end
+% 
+% for i= 1:7
+%     
+%     band = round(band + (randi([-150 150],1,19)./100)');
+%     [NewBand, RESNORM,EXITFLAG,OUTPUT] = fminunc('LiquidSnake',band,h,x,minY,msk,sigma,0,plim);
+%     if RESNORM <= resmin
+%         resmin = RESNORM;
+%         minBand = band;
+%     end
+% end
+% SolidSnake(minY,x,minBand,msk,sigma,4,plim);
 
 % now run the fitting
 % resmin = 4e+07;
 
-for i= 1:5
+for i= 1:10
 y = ones(dim, length(x)).*rand;
 
 %[NewY, RESNORM,EXITFLAG,OUTPUT] = fminunc('SolidSnake',y,h,x,band,SPLN,sigma,0,plim);
-[NewY, RESNORM,EXITFLAG,OUTPUT] = fminunc('SolidSnake',y,h,x,minBand,msk,sigma,0,plim);
+[NewY, RESNORM,~,~] = fminunc('SolidSnake',y,h,x,minBand,msk,sigma,0,plim);
     if RESNORM <= resmin
         resmin = RESNORM;
         minY = NewY;
@@ -70,9 +71,11 @@ y = ones(dim, length(x)).*rand;
 end 
 
 %SolidSnake(minY,x,NewBand,msk,sigma,4,plim);
+[NewY, RESNORM,EXITFLAG,OUTPUT] = fminunc('SolidSnake',minY,h,x,minBand,msk,sigma,0,plim);
 
 SolidSnake(minY,x,minBand,msk,sigma,4,plim);
 
+SolidSnake(NewY,x,minBand,msk,sigma,4,plim);
 
 
 %%
